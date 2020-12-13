a57edfeb4f71aedfaf28b16a4b765188ff8fcea7
SanderKit <75732340+SanderKit@users.noreply.github.com>
2020-12-14 00:54:04 +0200

    Delete IseseisevPython.py

--- IseseisevPython.py
+++
@@ -1,9 +0,0 @@
-arv = int(input("Pange sisse esimene number: "))
-arv2 = int(input("Pange sisse teine number: "))
-arvv = input("Kirjuta pluss märk: ")
-
-result = 0
-if arvv == "+":
-    result = arv + arv2
-    
-print(arv, "+", arv2, "=", result)
